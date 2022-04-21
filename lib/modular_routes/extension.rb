ffc44e96ce96472cb8a92a2d430030cb9c7516f0
vinibispo <vini.bispo015@gmail.com>
2022-04-20 22:56:42 -0300

    feat(extension): add implementation for extension file adding controller_method

--- lib/modular_routes/extension.rb
+++ lib/modular_routes/extension.rb
@@ -4,8 +4,9 @@ module ModularRoutes
   module Extension
     def modular_routes(**options, &block)
       api_only = options.fetch(:api_only, api_only?)
+      controller_method = options.fetch(:controller_method, 'call')
 
-      route_builder = Builder.new(api_only: api_only)
+      route_builder = Builder.new(api_only: api_only, controller_method: controller_method)
       route_builder.instance_eval(&block)
       route_builder.routes.each { |route| route.apply(self) }
     end
